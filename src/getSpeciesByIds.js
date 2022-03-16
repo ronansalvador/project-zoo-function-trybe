914cd6a42dbccb512aaa16cdef7c4fc6e686148a
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-16 15:04:38 -0300

    Requsitio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...param) {
+  const pesquisaPorId = data.species.filter((element) => param.includes(element.id)); // O método includes() determina se um array contém um determinado elemento
+  return pesquisaPorId;
 }
+
+console.log(data.species);
 
 module.exports = getSpeciesByIds;
