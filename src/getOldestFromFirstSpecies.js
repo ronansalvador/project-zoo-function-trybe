3e9960776e2a78ceb58ebbe3abbb595b0bad51fd
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-17 12:59:52 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const especies = data.species;
+const funcionarios = data.employees;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const pessoa = funcionarios.find((element) => element.id === id);
+  const animal = especies.find((element) => element.id === pessoa.responsibleFor[0]).residents;
+  const result = animal.sort((a, b) => b.age - a.age);
+  return Object.values(result[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
