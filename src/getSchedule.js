daf6ae93150b36a8069b33ed85678fe487a05012
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-17 19:27:51 -0300

    Requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const especies = data.species;
+const horas = data.hours;
+
+const agenda = () => {
+  const objeto = {};
+  Object.entries(horas).forEach((diasDaSemena) => {
+    const { open, close } = diasDaSemena[1];
+
+    Object.assign(objeto, {
+      [diasDaSemena[0]]: {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: (especies.filter((animais) =>
+          animais.availability.includes(diasDaSemena[0]))
+          .map((elemento) => elemento.name)),
+      },
+    });
+  });
+  objeto.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return objeto;
+};
+
+const retornaDias = (pesquisa) =>
+  ({ [pesquisa]: agenda()[pesquisa] });
+
+const disponibilidade = (pesquisa) => especies.find((element) =>
+  pesquisa === element.name).availability;
+
+function getSchedule(pesquisa) {
+  if (Object.keys(horas).includes(pesquisa)) return retornaDias(pesquisa);
+  if (especies.map((element) =>
+    element.name).includes(pesquisa)) return disponibilidade(pesquisa);
+
+  return agenda();
 }
 
 module.exports = getSchedule;
