3c5b2b7c1b4801e428fa7c2999d221079e043e6e
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-18 20:47:43 -0300

    Refatorando/reduzindo Requsito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animais = data.species.find((element) => element.name === animal);
-  const verifica = animais.residents.every((idade) => idade.age > age);
-  return verifica;
+  const animais = data.species.find((element) => element.name === animal)
+    .residents.every((idade) => idade.age > age);
+  return animais;
 }
 
 // console.log(getAnimalsOlderThan('otters', 7));
