fe91db2224e311d0fa90317c6e7215b0fd2f9f1f
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-18 21:41:39 -0300

    Adicionando comentarios ao Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,11 +4,10 @@ const especies = data.species;
 const funcionarios = data.employees;
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-  const pessoa = funcionarios.find((element) => element.id === id);
-  const animal = especies.find((element) => element.id === pessoa.responsibleFor[0]).residents;
-  const result = animal.sort((a, b) => b.age - a.age);
-  return Object.values(result[0]);
+  const pessoa = funcionarios.find((element) => element.id === id); // Busca Funcionario de acordo com o parametro id
+  const animal = especies.find((element) => element.id === pessoa.responsibleFor[0]).residents; // retorna a primeira especie
+  const result = animal.sort((a, b) => b.age - a.age); // ordena de forna decrescente
+  return Object.values(result[0]); // retorna o indice [0] apos ordenar
 }
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
