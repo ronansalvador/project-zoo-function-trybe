45d68c3162d6e796bdf7ca42da3d1a9e8e6a6bb3
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-03-18 21:42:20 -0300

    Removendo console.log

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -9,5 +9,5 @@ function getOldestFromFirstSpecies(id) {
   const result = animal.sort((a, b) => b.age - a.age); // ordena de forna decrescente
   return Object.values(result[0]); // retorna o indice [0] apos ordenar
 }
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
 module.exports = getOldestFromFirstSpecies;
